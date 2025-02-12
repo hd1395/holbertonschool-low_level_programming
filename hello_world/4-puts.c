942cffef2624a41b7dbe6368be20bc3ecd1d99b2
Hamad <hdahash@hotmail.com>
2025-02-12 09:25:40 +0000

    Fix Task #4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - print string using puts
+ * Return: 0
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
