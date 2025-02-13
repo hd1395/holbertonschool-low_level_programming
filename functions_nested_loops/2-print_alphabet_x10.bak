a2bb93ce06a375b06298d27e7b5e16be800b62af
Hamad <hdahash@hotmail.com>
2025-02-13 10:41:00 +0000

    Fix #1 Task #2

---
+++ functions_nested_loops/2-print_alphabet_x10.bak
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,20 @@
  * main - prints _putchar, followed by a new line.
  * Return: 0
  */
-int main(void)
+/*int main(void)
+ *{
+ *print_alphabet_x10();
+ *return (0);
+ *}
+ */
+void print_alphabet_x10(void)
 {
-print_alphabet_x10();
-return (0);
+int i;
+char c;
+for (i = 1; i <= 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
 }
