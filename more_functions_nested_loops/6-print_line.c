1c909fe2ff54e2fabe95e7d2f3dc7f520cd5d00b
Hamad <hdahash@hotmail.com>
2025-02-23 06:02:58 +0000

    Task #6 : print straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ */
+
+void print_line(int n)
+{
+
+for (i = 0 ; i < n ; i++)
+_putchar('_');
+
+_putchar('\n');
+}
