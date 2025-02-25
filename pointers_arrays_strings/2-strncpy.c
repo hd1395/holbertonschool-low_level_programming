e3f7f84ed78181bcada754b57cb952e7a377a7b8
Hamad <hdahash@hotmail.com>
2025-02-25 09:17:53 +0000

    Fix #2: add _strlen to the file

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -26,3 +26,20 @@ dest++;
 }
 return (_dest);
 }
+/**
+ * _strlen - calculate the length of a string.
+ * @s: String to find the length of.
+ *
+ * Return: the length of the string.
+ *
+ */
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != '\0')
+{
+l++;
+s++;
+}
+return (l);
+}
