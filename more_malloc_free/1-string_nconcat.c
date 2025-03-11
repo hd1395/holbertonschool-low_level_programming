abce49dfaf3150610bd52176286156d51d2659d0
Hamad <hdahash@hotmail.com>
2025-03-11 21:35:54 +0000

    Fix the last character and if any of strings is NULL

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,21 @@
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
-/**
- * min - find the minimum values of two numbers.
- * 
- * @n1: first number.
- * @n2: second number.
- *
- * Return: the minimum number of n1 and n2
- */
-int min(int n1, int n2)
-{
-if (n1 <= n2)
-return (n1);
-return (n2);
-}
+
 /**
  * str_len - counts the string charcters.
  * @s: string to calculate the length for
  *
  * Return: number of charcters of string s.
  */
-int str_len(char *s)
+unsigned int str_len(char *s)
 {
-if (!s)
-return (0);
 if (!*s)
 return (0);
 return (1 + str_len(s + 1));
 }
+
 /**
  * string_nconcat - concatenates two strings.
  * @s1: first string
@@ -38,17 +26,24 @@ return (1 + str_len(s + 1));
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int i, s1_len, s2_len;
+unsigned int i, s1_len, s2_len;
 char *s;
 s1_len = str_len(s1);
 s2_len = str_len(s2);
-s = malloc(sizeof(char) * (s1_len + min(s2_len, n) + 1));
-if (!s)
-return (0);
+if (s2_len <= n)
+n = s2_len;
+s = malloc(sizeof(char) * (s1_len + n + 1));
+if (s == NULL)
+return (NULL);
 for (i = 0; i < s1_len; i++)
-s[i] = *(s1++);
-for (i = s1_len; i < s1_len + min(s2_len, n); i++)
-s[i] = *(s2++);
-s[s1_len + min(s2_len, n) + 1] = '\0';
+{
+s[i] = s1[i];
+}
+for (i = s1_len; i < s1_len + n; i++)
+{
+s[i] = s2[i - s1_len];
+}
+s[s1_len + n] = '\0';
 return (s);
+
 }
