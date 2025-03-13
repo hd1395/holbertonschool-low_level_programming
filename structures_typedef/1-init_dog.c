bc34b080d5ce56d982207272d3df82ea0150ba5f
Hamad <hdahash@hotmail.com>
2025-03-13 07:35:32 +0000

    add files README.md dog.h 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog.
+ *
+ * @d: variable to initialize.
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog
+ *
+ * Return: pointer to a new array with numbers from min to max
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
