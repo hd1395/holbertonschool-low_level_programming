456bd257058ac4f69d0c52d5d94a1bcaa14d301d
Hamad <hdahash@hotmail.com>
2025-03-13 08:55:03 +0000

    update dog.h add 5-free_dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_dog - frees dog.
+ *
+ * @d: dog to free
+ *
+ */
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
