d688b04bff92a0476c0e5134e299f215dc48b899
Hamad <hdahash@hotmail.com>
2025-03-16 07:45:18 +0000

    fix print_name issue

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,21 +2,6 @@
 #include <stdio.h>
 
 /**
- * printname - prints name.
- * @name: name to print.
- *
- */
-void printname(char *name)
-{
-int i;
-for (i = 0; name[i]; i++)
-{
-_putchar(name[i]);
-}
-_putchar('\n');
-}
-
-/**
  * print_name - prints name.
  * @name: name to print.
  * @f: function to print name
