bb8ad684253de1ff70640c09d85e96e2946075d6
Hamad <Hdahash@hotmail.com>
2025-03-18 10:18:16 +0000

    fix issue

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,30 +11,34 @@
 void print_all(const char * const format, ...)
 {
 va_list ap;
-unsigned int i;
+unsigned int i, f;
 char *s;
 va_start(ap, format);
 i = 0;
-
+f = 0;
 while (format && format[i])
 {
 if ((format[i] == 'c' || format[i] == 'f' || format[i] == 'i'
-|| format[i] == 's') && i > 0)
+|| format[i] == 's') && f)
 printf(", ");
 switch (format[i])
 {
 case 'c':
 printf("%c", va_arg(ap, int));
+f = 1;
 break;
 case 'i':
 printf("%d", va_arg(ap, int));
+f = 1;
 break;
 case 'f':
 printf("%f", va_arg(ap, double));
+f = 1;
 break;
 case 's':
 s = va_arg(ap, char *);
 printf("%s", s ? s : "(nil)");
+f = 1;
 break;
 }
 i++;
