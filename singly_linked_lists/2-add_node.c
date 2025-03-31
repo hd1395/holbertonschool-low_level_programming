a859fce4b1681793ee6d0e399ce5ddfaf2bcf2ca
Hamad <hdahash@hotmail.com>
2025-03-31 07:39:27 +0000

    use custom strlen 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,6 +4,20 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: String to calculate length for.
+ *
+ * Return: the length of a string.
+ */
+
+int _strlen(const char *s)
+{
+if (*s)
+return (1 + _strlen(s + 1));
+return (0);
+}
+
+/**
  * add_node - adds a new node at the beginning of a linked list_t list
  * @head: pointer to the head of the list
  * @str: string data
@@ -21,7 +35,7 @@ printf("Error\n");
 return (NULL);
 }
 new_node->str = strdup(str);
-new_node->len = strlen(str);
+new_node->len = _strlen(str);
 new_node->next = *head;
 *head = new_node;
 return (new_node);
