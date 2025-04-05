e75da5d290f3c9b5f3625bc39fa6870194bf93c4
Hamad <Hdahash@hotmail.com>
2025-04-05 08:07:03 +0300

    fix styling issue in 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -6,7 +6,7 @@
  *                            a dlistint_t linked list
  * @head: pointer to the head of the list
  * @index: index of the node that should be deleted. Index starts at 0
- * 
+ *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
