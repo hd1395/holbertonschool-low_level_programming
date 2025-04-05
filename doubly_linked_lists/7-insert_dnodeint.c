d0877ff7011fbab83e8fc807c3df776904068e63
Hamad <Hdahash@hotmail.com>
2025-04-05 07:48:30 +0300

    fix the issue with 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,52 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to the head of the list
- * @idx: index to insert at
- * @n: data to add
- *
- * Return:  the newly created node
+ * insert_node - insert node at given index
+ * @node: pointer to nth position node in doubly linked list
+ * @n: node data
+ * Return: pointer to the newly inserted node
  */
+dlistint_t *insert_node(dlistint_t *node, int n)
+{
+dlistint_t *new_node;
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-unsigned int i = 0;
-dlistint_t *new_node, *temp = *h;
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-{
-printf("Error\n");
+new_node = malloc(sizeof(struct dlistint_s));
+if (!new_node)
 return (NULL);
-}
 new_node->n = n;
-while (temp)
-{
-if (i == idx - 1)
-{
-new_node->next = temp->next;
-temp->next = new_node;
-new_node->prev = temp;
+new_node->next = node;
+new_node->prev = node->prev;
+node->prev->next = new_node;
+node->prev = new_node;
 return (new_node);
 }
-temp = temp->next;
-i++;
+
+/**
+ * insert_dnodeint_at_index - create and insert node at nth index
+ * @h: pointer to the head of the list
+ * @idx: index to insert at
+ * @n: node to add
+ * Return: pointer to the newly inserted node, or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *tmp;
+
+if (idx == 0)
+return (add_dnodeint(h, n));
+if (!h)
+return (NULL);
+
+tmp = *h;
+while ((idx != 0) && (tmp->next))
+{
+idx -= 1;
+tmp = tmp->next;
+if (idx == 0)
+return (insert_node(tmp, n));
 }
+
+if (idx == 1)
+return (add_dnodeint_end(h, n));
 return (NULL);
 }
